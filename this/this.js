fe2ca9cf3921c4313db910a6520023770726d86d
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-05-06 09:23:16 +0330

    arrow-function: extract functio

--- this/this.js
+++ this/this.js
@@ -3,11 +3,16 @@ const person = {
   greet: function () {
     console.log("Hello " + this.name);
   },
+  greetArrowFunction: () => {
+    console.log("Hello " + this.name);
+  },
 };
 
 /* case 1 */
 const greet = person.greet; // extract the function from person
 greet(); /* Invoking greet() uses default binding, so this refers to the global object (window in browsers). */
+const arrow = person.greetArrowFunction;
+arrow();
 
 /* case 2 */
 person.greet(); // prints Hello Ali
