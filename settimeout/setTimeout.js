2cb29592307817032ac274d3fc18f18a7b7f666e
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2025-05-09 16:01:30 +0330

    settimeout

---
+++ settimeout/setTimeout.js
@@ -0,0 +1,43 @@
+function myTimeout(callback, delay, ...args) {
+  let clear = false;
+  const start = Date.now();
+
+  function inner() {
+    if (clear) return;
+    const elapsed = Date.now() - start;
+    if (elapsed >= delay) {
+      callback(...args);
+      clearInterval(intervalId);
+    }
+  }
+
+  const intervalId = setInterval(() => {
+    inner();
+  }, 100);
+
+  return {
+    clear: () => {
+      clearInterval(intervalId);
+      clear = true;
+    },
+  };
+}
+
+const callback = (args) => console.log("proper time passed, args: " + args);
+
+const timeout = myTimeout(callback, 2000, "meow");
+
+setTimeout(() => {
+  const { clear } = timeout;
+  clear();
+  console.log("cleared");
+}, 1000);
+
+// code below did not worked because of diffrent instances
+// myTimeout(callback, 2000, "meow");
+
+// setTimeout(() => {
+//   const { clear } = myTimeout();
+//   clear();
+//   console.log("cleared");
+// }, 1000);
