c75213f4fdc139ffa363c53753e9d91a59a6a5a3
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2025-06-05 23:26:30 +0330

    Create typeof.js

---
+++ typeof/typeof.js
@@ -0,0 +1,11 @@
+const nullType = typeof null;
+console.log(nullType); // object
+console.log(typeof nullType); // string
+
+console.log(typeof undefined); //undefined
+
+console.log(undefined == null);
+console.log(undefined === null);
+
+console.log(null && false && undefined);
+console.log("Alex" && "Meow" && "Ali");
