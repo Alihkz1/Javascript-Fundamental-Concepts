ddaa18b64d5488c6f31cb0c61046c0686873200e
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-06-21 11:48:36 +0330

    nested array generator algorithm

--- algorithm/leetcode/nested-array-generator.js
+++ algorithm/leetcode/nested-array-generator.js
@@ -2,13 +2,31 @@
  * @param {Array} arr
  * @return {Generator}
  */
-var inorderTraversal = function*(arr) {
-    
+var inorderTraversal = function* (arr) {
+  const result = [];
+  let index = 0;
+  function* flatten(toFlatArray) {
+    toFlatArray.forEach((el) => {
+      if (typeof el === "number") result.push(el);
+      else flatten(el);
+    });
+  }
+  flatten(arr);
+  yield result[index];
+  index++;
 };
 
-/**
- * const gen = inorderTraversal([1, [2, 3]]);
- * gen.next().value; // 1
- * gen.next().value; // 2
- * gen.next().value; // 3
- */
+var inorderTraversal = function* (arr) {
+  function* flatten(toFlatArray) {
+    for (const el of toFlatArray) {
+      if (typeof el === "number") yield el;
+      else yield* flatten(el);
+    }
+  }
+  yield* flatten(arr);
+};
+
+const gen = inorderTraversal([1, [2, 3]]);
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next().value); // 3
