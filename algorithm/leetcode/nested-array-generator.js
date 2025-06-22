cc35252fbabc3585ae115a8fb1a3eb8f97cc157a
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2025-06-22 13:13:44 +0330

    algorithm -> array proto last

--- algorithm/leetcode/nested-array-generator.js
+++ algorithm/leetcode/nested-array-generator.js
@@ -2,19 +2,19 @@
  * @param {Array} arr
  * @return {Generator}
  */
-var inorderTraversal = function* (arr) {
-  const result = [];
-  let index = 0;
-  function* flatten(toFlatArray) {
-    toFlatArray.forEach((el) => {
-      if (typeof el === "number") result.push(el);
-      else flatten(el);
-    });
-  }
-  flatten(arr);
-  yield result[index];
-  index++;
-};
+// var inorderTraversal = function* (arr) {
+//   const result = [];
+//   let index = 0;
+//   function* flatten(toFlatArray) {
+//     toFlatArray.forEach((el) => {
+//       if (typeof el === "number") result.push(el);
+//       else flatten(el);
+//     });
+//   }
+//   flatten(arr);
+//   yield result[index];
+//   index++;
+// };
 
 var inorderTraversal = function* (arr) {
   function* flatten(toFlatArray) {
