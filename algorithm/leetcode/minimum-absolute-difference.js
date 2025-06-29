7e7f9e0ac9b435da77da0a5ec241da782a479e58
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-06-29 13:12:58 +0330

    Update minimum-absolute-difference.js

--- algorithm/leetcode/minimum-absolute-difference.js
+++ algorithm/leetcode/minimum-absolute-difference.js
@@ -3,15 +3,24 @@
  * @return {number[][]}
  */
 var minimumAbsDifference = function (arr) {
+  const result = [];
   const sorted = arr.sort((a, b) => a - b);
   const diffArr = [];
-  for (i = 0; i < sorted.length; i++) {
+  for (let i = 0; i < sorted.length; i++) {
     let diff = Math.abs(sorted[i] - sorted[i + 1]);
     if (!Number.isNaN(diff)) diffArr.push(diff);
   }
-  const subArrSize = Math.min(...diffArr);
-  console.log(subArrSize);
+  const FinalDiff = Math.min(...diffArr);
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] - sorted[i - 1] === FinalDiff) {
+      result.push([sorted[i - 1], sorted[i]]);
+    }
+  }
+  console.log(result);
+  return result;
 };
 
-const input = [3,8,-10,23,19,-4,-14,27];
+const input = [3, 8, -10, 23, 19, -4, -14, 27];
+const input2 = [1, 3, 6, 10, 15];
 minimumAbsDifference(input);
+minimumAbsDifference(input2);
