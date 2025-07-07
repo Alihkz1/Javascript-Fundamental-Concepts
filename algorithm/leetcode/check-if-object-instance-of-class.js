c880f78ba0e597094f1f01b5e7c68a74653c02c8
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-07-07 13:20:39 +0330

    Create check-if-object-instance-of-class.js

---
+++ algorithm/leetcode/check-if-object-instance-of-class.js
@@ -0,0 +1,20 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+  if (obj === null || obj === undefined) return false;
+  if (
+    (typeof obj === "number" && classFunction === Number) ||
+    (typeof obj === "string" && classFunction === String) ||
+    (typeof obj === "bigint" && classFunction === BigInt) ||
+    (typeof obj === "boolean" && classFunction === Boolean)
+    (typeof obj === "object" && classFunction)
+  ) {
+    return true;
+  }
+  return obj instanceof classFunction;
+};
+
+checkIfInstanceOf(new Date(), Date); // true
