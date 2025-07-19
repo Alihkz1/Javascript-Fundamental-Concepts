4e562e347c715af1f9102474281f340c0f510dcf
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-07-19 11:12:00 +0330

    Update design-hashmap.js

--- algorithm/leetcode/design-hashmap.js
+++ algorithm/leetcode/design-hashmap.js
@@ -1,28 +1,33 @@
-var MyHashMap = function () {};
+var MyHashMap = function () {
+  this.array = new Array(1000001); // Because key is <= 10^6
+};
 
 /**
  * @param {number} key
  * @param {number} value
  * @return {void}
  */
-MyHashMap.prototype.put = function (key, value) {};
+MyHashMap.prototype.put = function (key, value) {
+  this.array[key] = value;
+};
 
 /**
  * @param {number} key
  * @return {number}
  */
-MyHashMap.prototype.get = function (key) {};
+MyHashMap.prototype.get = function (key) {
+  return this.array[key] === undefined ? -1 : this.array[key];
+};
 
 /**
  * @param {number} key
  * @return {void}
  */
-MyHashMap.prototype.remove = function (key) {};
+MyHashMap.prototype.remove = function (key) {
+  this.array[key] = undefined;
+};
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * var obj = new MyHashMap()
- * obj.put(key,value)
- * var param_2 = obj.get(key)
- * obj.remove(key)
- */
+// var obj = new MyHashMap();
+// obj.put(10, 20);
+// var param_2 = obj.get(key);
+// obj.remove(key);
