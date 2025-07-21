7940130e2bc260620cc2fd6b57b9490416297ba1
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2025-07-21 09:50:22 +0330

    Intersection Observer

--- intersection-observer/main.js
+++ intersection-observer/main.js
@@ -1,25 +1,39 @@
-// It's a browser API that allows you to asynchronously
-//  observe changes in the intersection (visibility) of a target element
-// with an ancestor element or the viewport.
+// The IntersectionObserver API lets you asynchronously observe changes in the
+//  intersection of a target element with an ancestor element or the viewport.
+//  It's useful for:
+// Lazy-loading images
+// Infinite scroll
+// Triggering animations
+// Reporting ad viewability
+// Analytics on elements visibility
+// You can also pass an array: threshold: [0, 0.5, 1]. callback fires up each time
 
-// IntersectionObserver is much more
-// efficient than using scroll or resize events for visibility checks.
-
-const targetElement = document.getElementById("lazyImage");
-
-callback = (entries, observer) => {
+const options = {
+  root: document.querySelector("#box"),
+  rootMargin: 0,
+  threshold: 1, // Callback fires when 50% or more of the element enters or exits the view.
+};
+const callback = (entries, obs) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting) console.log("target element is intersecting");
+    if (entry.isIntersecting) {
+      console.log(entry.target);
+      console.log("gurba is intersecting");
+      const img = entry.target;
+      img.alt = "loading";
+      setTimeout(() => {
+        img.src =
+          "https://www.fearfreehappyhomes.com/wp-content/uploads/2020/08/bigstock-Closeup-Meowing-Ginger-Cat-On-97254212.jpg";
+      }, 1000);
+      setTimeout(() => {
+        // img.src = img.dataset.src;
+        obs.unobserve(img); // Stop observing after loading
+      }, 5000);
+    }
   });
 };
 
-const observer = new IntersectionObserver(callback, {
-  root: null, // root: The element used as the viewport. Use null for the browser viewport.
-  threshold: 0.5, // trigger when 50% visible
-});
+const box = document.getElementById("box");
+const gurba = document.getElementById("gurba");
 
-observer.observe(targetElement);
-
-// stop observing
-observer.unobserve(targetElement); // solution 1
-observer.disconnect(); // solution 2
+const observer = new IntersectionObserver(callback);
+observer.observe(gurba);
